ea344a75a451aa6a4aff59be291a7cecdf2e2e76
viniciusousazup <vinicius.sousa@zup.com.br>
2020-06-25 19:07:15 -0300

    writefile

--- scaffold/coffee-go/src/main.go
+++ scaffold/coffee-go/src/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"coffee/pkg/coffee"
+	"io/ioutil"
 	"os"
 	"strconv"
 )
 
-func main(){
+func main() {
 	coffee.GiveMeSomeCoffee(loadInputs())
 }
 
@@ -14,9 +15,12 @@ func loadInputs() coffee.Inputs {
 	delivery, _ := strconv.ParseBool(os.Getenv("DELIVERY"))
 	name := os.Getenv("NAME")
 	coffeeType := os.Getenv("COFFEE_TYPE")
+	output := os.Getenv("RIT_OUTPUT_FILE")
+	ioutil.WriteFile(output, []byte("V1=a;V2=b;V3=c;V4=d"), 0755)
+
 	return coffee.Inputs{
-		Name: name,
+		Name:       name,
 		CoffeeType: coffeeType,
-		Delivery: delivery,
+		Delivery:   delivery,
 	}
 }
